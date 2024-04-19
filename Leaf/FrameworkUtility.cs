477bbf08429db218e1632be16f9e4344e68f1069
Nikolche Kolev <nikolce1kolev@gmail.com>
2024-04-19 11:45:16 -0700

    Merging ProjectReferences

---
+++ Leaf/FrameworkUtility.cs
@@ -0,0 +1,12 @@
+﻿using NuGet.Frameworks;
+
+namespace Leaf;
+
+public class FrameworkUtility
+{
+
+    public NuGetFramework GetFramework()
+    {
+        return FrameworkConstants.CommonFrameworks.Net50;
+    }
+}
---
+++ Lib/SDK.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Text.Json.Nodes;
+
+namespace Lib;
+
+public class SDK
+{
+    public static string GetFramework()
+    {
+        var json = new JsonObject();
+        return (new Leaf.FrameworkUtility()).GetFramework().ToString();
+    }
+}
